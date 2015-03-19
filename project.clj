d57ceeb19859e954fa536a0321295645440a1c80
Takahiro Noda <takahiro.noda+github@gmail.com>
2015-03-19 18:16:20 +0900

    Update the homepage url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.tnoda/incanter.read-xlsx "0.0.7"
   :description "Provide a Incanter function to read Excel 2007 and Excel 97/2000/XP/2003 file formats"
-  :url "https://github.com/tnoda/tnoda.incanter.xlsx"
+  :url "https://github.com/tnoda/tnoda.incanter.read-xlsx"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
